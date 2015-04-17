bb53e6e0af151383a14c7d66f9e1fe2a8261d423
1024jp <1024jp@wolfrosch.com>
2015-04-17 18:54:48 +0900

    Remove duplicated codes

--- track.py
+++ track.py
@@ -26,72 +26,61 @@ def init_detector():
 
 def main(file_path):
     time = 0.0
+    last_index = -1
+    points = dict()
+
     init_detector()
 
     # load a movie file
-    file_name = os.path.basename(file_path)
     movie = moviefile.Movie(file_path)
 
     # open a window
-    image = movie.load_image(time)
+    file_name = os.path.basename(file_path)
     window = gui.Window(file_name)
-    window.image = image
-    window.display()
-
-    # click heads' positions on the first frame
     eventListener = gui.EventListener(window)
-    clicked_points = eventListener.get_xy()
-
-    last_index = len(clicked_points) - 1
-    points = dict(zip(range(len(clicked_points)), clicked_points))
-
-    # output
-    for idx, point in points.items():
-        _dump_result(time, idx, point.x, point.y)
 
     # process each frame
     while True:
-        time += TIME_STEP
-
-        # load images
-        prev_image = movie.load_image(time - TIME_STEP)
         image = movie.load_image(time)
-        if prev_image is None or image is None:
+        if image is None:
             break
 
         window.image = image
-        detector = PatternDetector(prev_image, image)
 
-        for idx, point in points.items():
-            # find similar pattern to the current frame from the next frame
-            try:
-                dx, dy = detector.detect(point.x, point.y)
-            except ValueError:  # frame out
-                points.pop(idx, None)
-                continue
+        # auto-track points
+        if points:
+            prev_image = movie.load_image(time - TIME_STEP)
+            if prev_image is None:
+                break
+            detector = PatternDetector(prev_image, image)
 
-            # translate position
-            point.move(dx, dy)
+            for idx, point in points.items():
+                try:
+                    dx, dy = detector.detect(point.x, point.y)
+                except ValueError:  # frame out
+                    points.pop(idx, None)
+                    continue
 
-            # raise flag
-            point.isAutoDetected = True
+                point.move(dx, dy)
+                point.isAutoDetected = True
 
-            # draw marker
-            window.draw_marker(point.x, point.y, PATTERN_SIZE)
+                window.draw_marker(point.x, point.y, PATTERN_SIZE)
 
         window.display()
 
         # wait for mouse event
-        new_points = eventListener.get_xy()
+        clicked_points = eventListener.get_xy()
 
         # append new points
-        for point in new_points:
+        for point in clicked_points:
             last_index += 1
             points[last_index] = point
 
         # output
         for idx, point in points.items():
             _dump_result(time, idx, point.x, point.y)
+
+        time += TIME_STEP
 
     window.close()
 
